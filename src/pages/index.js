184b1ef1c9dcef08a08d6b3f73b468a3cb987b63
lujinhui <lujinhui@hotmail.com>
2018-07-26 00:21:34 +0800

    add Designer and Coder text

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ const IndexPage = () => (
     <h1>Learn to <br/> design and code React apps</h1>
     <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
     <p>Now go build something great.</p>
+    <p>Designer and Coder</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
